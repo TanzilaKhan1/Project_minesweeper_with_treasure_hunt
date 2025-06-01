4e9f472176b5bbd4969b9eca212636ebba1ee8f7
Tanzila Khan <149196723+TanzilaKhan1@users.noreply.github.com>
2025-06-02 00:45:34 +0600

    Update customized.cpp

--- minesweeper/customized.cpp
+++ minesweeper/customized.cpp
@@ -31,15 +31,15 @@ void customized::on_Done_Button_clicked()
    int columns = getColumns();
    int mines = getMines();
    // Ensure that the number of rows, columns and mines are within the allowed range
-   if (rows >= 3 && columns>=3 && mines >= rows * columns * 0.12) {
+   if (rows >= 3 && columns>=3 && mines >= rows * columns * 0.12 && mines <= rows * columns * 0.75) {
        accept();}
    if(rows < 3 || columns < 3){
        // Show a warning message
        QMessageBox::warning(this, "Warning", "Number of rows and columns should be at least 3.");
    }
-   if (mines < rows * columns * 0.12) {
+   if (mines < rows * columns * 0.12 || mines > rows * columns * 0.75) {
        // Show a warning message
-       QMessageBox::warning(this, "Warning", "Number of mines should be at least 12% of the total cells.");
+       QMessageBox::warning(this, "Warning", "Number of mines should be at least 12% and at most 75% of the total cells.");
    }
 }
 
